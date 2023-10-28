644bf92c47deb1d3e78f5887f4b39ad633b38817
Aleksey Sidorov <alesh.sidorov2017@yandex.ru>
2023-10-28 21:53:50 +0300

    Добавил аннотацию для настроек по дефолту драйвера для каждого теста

--- src/test/java/sidorov/aleksey/tests/TestBase.java
+++ src/test/java/sidorov/aleksey/tests/TestBase.java
@@ -1,5 +1,12 @@
 package sidorov.aleksey.tests;
 
+import org.junit.jupiter.api.BeforeAll;
+import sidorov.aleksey.helpers.DriverSetting;
+
 public class TestBase {
 
+    @BeforeAll
+    static void beforeAll() {
+        DriverSetting.configureDriver();
+    }
 }
