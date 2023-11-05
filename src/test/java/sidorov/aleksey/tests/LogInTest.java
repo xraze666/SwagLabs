c7321c2e7e54f5616daf204f9cb566d29250455c
Aleksey Sidorov <alesh.sidorov2017@yandex.ru>
2023-11-05 23:03:03 +0300

    Объявил класс главной страницы, вынес методы в чейны

--- src/test/java/sidorov/aleksey/tests/LogInTest.java
+++ src/test/java/sidorov/aleksey/tests/LogInTest.java
@@ -1,12 +1,26 @@
 package sidorov.aleksey.tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import sidorov.aleksey.pages.LoginPage;
+import sidorov.aleksey.pages.MainPage;
 
-import static com.codeborne.selenide.Selenide.open;
 
-public class LogInTest {
+public class LogInTest extends TestBase{
+    LoginPage loginPage = new LoginPage();
+    MainPage mainPage = new MainPage();
     @Test
-    public void login(){
-        open("https://www.saucedemo.com/");
+    public void login()
+    {
+        loginPage
+                .openLoginPage()
+                .loginPageTitle.should(Condition.visible);
+        loginPage.userName.setValue(loginPage.getCreds(loginPage.loginCredentials));
+        loginPage.password.setValue(loginPage.getCreds(loginPage.passwordCredentials));
+        loginPage.loginButton.click();
+
+        mainPage
+                .getPageHeader()
+                .checkTitle("Swag Labs");
     }
 }
