c9ab2284bae264915f619ae0aaffed87eb51f504
Aleksey Sidorov <alesh.sidorov2017@yandex.ru>
2023-11-05 23:01:34 +0300

    Описал главную страницу и общий хедер

---
+++ src/test/java/sidorov/aleksey/pageElements/PageHeader.java
@@ -0,0 +1,21 @@
+package sidorov.aleksey.pageElements;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PageHeader {
+    public SelenideElement headerContainer = $("#header_container");
+    public SelenideElement title = headerContainer.find(".app_logo");
+    public SelenideElement shoppingCartContainer = headerContainer.find("#shopping_cart_container");
+    public SelenideElement shoppingCartBadge = shoppingCartContainer.find(".shopping_cart_badge");
+    public SelenideElement burgerMenuButton = headerContainer.find(".bm-burger-button");
+
+    public void checkTitle(String title){
+        this.title.shouldHave(text(title));
+    }
+    public void checkBadgeCount(String count){
+        shoppingCartBadge.shouldHave(text(count));
+    }
+}
---
+++ src/test/java/sidorov/aleksey/pages/MainPage.java
@@ -0,0 +1,10 @@
+package sidorov.aleksey.pages;
+
+import sidorov.aleksey.pageElements.PageHeader;
+
+public class MainPage {
+    private final PageHeader pageHeader = new PageHeader();
+    public PageHeader getPageHeader(){
+        return pageHeader;
+    }
+}
