ccd8ca6e32ed08c7d4f970b87244bd75d80a703d
Aleksey Sidorov <alesh.sidorov2017@yandex.ru>
2023-11-06 16:36:45 +0300

    Подключил аллюр степы, добавил разметки отчета

--- src/test/java/sidorov/aleksey/tests/LogInTest.java
+++ src/test/java/sidorov/aleksey/tests/LogInTest.java
@@ -1,26 +1,40 @@
 package sidorov.aleksey.tests;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sidorov.aleksey.pages.LoginPage;
 import sidorov.aleksey.pages.MainPage;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class LogInTest extends TestBase{
     LoginPage loginPage = new LoginPage();
     MainPage mainPage = new MainPage();
     @Test
+    @DisplayName("Login with correct login/password")
+    @Severity(SeverityLevel.CRITICAL)
     public void login()
     {
-        loginPage
-                .openLoginPage()
-                .loginPageTitle.should(Condition.visible);
-        loginPage.userName.setValue(loginPage.getCreds(loginPage.loginCredentials));
-        loginPage.password.setValue(loginPage.getCreds(loginPage.passwordCredentials));
-        loginPage.loginButton.click();
+        step("Открываем страницу авторизации", ()->{
+            loginPage
+                    .openLoginPage()
+                    .loginPageTitle.should(Condition.visible);
+        });
 
-        mainPage
-                .getPageHeader()
-                .checkTitle("Swag Labs");
+        step("Ввод учетных данных указанных на странице",()->{
+            loginPage.userName.setValue(loginPage.getCreds(loginPage.loginCredentials));
+            loginPage.password.setValue(loginPage.getCreds(loginPage.passwordCredentials));
+            loginPage.loginButton.click();
+        });
+
+        step("Проверка отображения главной страницы приложения",()->{
+            mainPage
+                    .getPageHeader()
+                    .checkTitle("Swag Labs");
+        });
     }
 }
