94a50f36743d2c571f4f7f7017febda0230b0f27
Aleksey Sidorov <alesh.sidorov2017@yandex.ru>
2023-11-12 16:21:07 +0300

    Подключил селенид логгер для вложений, реализоавл метод скриншота в момент падения/завершения теста

--- src/test/java/sidorov/aleksey/tests/TestBase.java
+++ src/test/java/sidorov/aleksey/tests/TestBase.java
@@ -1,12 +1,25 @@
 package sidorov.aleksey.tests;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import sidorov.aleksey.helpers.AllureAttachments;
 import sidorov.aleksey.helpers.DriverSetting;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
         DriverSetting.configureDriver();
+    }
+    @AfterEach
+    public void addAttachments() {
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        Selenide.closeWebDriver();
     }
 }
